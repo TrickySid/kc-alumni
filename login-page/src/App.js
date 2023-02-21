f8e639eaba3c79247bcdf8cd0fa74cf793c852e2
Siddhesh Bakre <85791341+TrickySid@users.noreply.github.com>
2023-02-21 18:46:40 +0530

    Add files via upload

---
+++ login-page/src/App.js
@@ -0,0 +1,11 @@
+import Headers from "./components/Headers";
+import Details from "./components/Details";
+
+export default function App() {
+  return (
+    <div>
+      <Headers />
+      <Details />
+    </div>
+  );
+}
