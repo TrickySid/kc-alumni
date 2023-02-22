68e747f03afd9d176a64b2e421c993dba01e1690
Siddhesh Bakre <85791341+TrickySid@users.noreply.github.com>
2023-02-22 17:38:49 +0530

    Add files via upload

---
+++ login-page/src/components/Headers.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Headers.css";
+
+export default function Headers() {
+  return (
+    <div className="headers">
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/ycug9kepc8q-162%3A24?alt=media&token=13a935c1-f388-411a-ade2-fe9f21475ea5"
+        alt="logo"
+        className="clg-logo"
+      />
+      <p className="main-header">KCCEMSR ALUMNI</p>
+      <p className="sub-header">Connect, Interact, Grow</p>
+    </div>
+  );
+}
